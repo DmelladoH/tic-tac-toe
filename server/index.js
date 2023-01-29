670a324534eafae213e1568d2684eb3fd6b9afaf
DmelladoH <mellado.m2@gmail.com>
2023-01-29 13:41:42 +0100

    add server logic

--- server/index.js
+++ server/index.js
@@ -1,7 +1,34 @@
 const express = require('express')
-const app = express()
-app.use(express.json())
+const cors = require('cors')
 
-const server = app.listen('3000', () => {
-  console.log('Server running on port 3000')
+const PORT = 3000
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*'
+  }
+})
+
+io.on('connection', (socket) => {
+  socket.on('joinRoom', (code) => {
+    console.log(`A user has join room ${code}`)
+    socket.join(code)
+  })
+
+  socket.on('play', ({ coord, roomCode }) => {
+    console.log(`play at ${coord} to ${roomCode}`)
+    socket.broadcast.to(roomCode).emit('updateGame', coord)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User Disconnected')
+  })
 })
