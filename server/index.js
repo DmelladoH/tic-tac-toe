9621be2cb4839de1560ca993db23132d9fb5bc07
DmelladoH <mellado.m2@gmail.com>
2023-03-19 17:22:58 +0100

    add server

--- server/index.js
+++ server/index.js
@@ -19,13 +19,36 @@ const io = require('socket.io')(server, {
 
 io.on('connection', (socket) => {
   socket.on('joinRoom', (code) => {
-    console.log(`A user has join room ${code}`)
+    const rooms = io.sockets.adapter.rooms
+    const room = rooms.get(code)
+
+    if (room && room.size >= 2) {
+      console.log(`Room ${code} is full`)
+      socket.emit('roomFull')
+      return
+    }
+
+    // create room if not exist
+    if (room === undefined) {
+      socket.join(code)
+      socket.emit('roomJoined', { player: 'X' })
+      io.to(code).emit('gameWaitingState', { waiting: true })
+
+      console.log('room created for player X to room', code)
+      return
+    }
+
+    // Join room if exist
     socket.join(code)
+    socket.emit('roomJoined', { player: 'O' })
+    console.log('room joined for player O to room', code)
+
+    io.to(code).emit('gameWaitingState', { waiting: false })
   })
 
-  socket.on('play', ({ coord, roomCode }) => {
-    console.log(`play at ${coord} to ${roomCode}`)
-    socket.broadcast.to(roomCode).emit('updateGame', coord)
+  socket.on('play', async ({ player, board, roomCode }) => {
+    console.log(`player ${player} play at ${board} to ${roomCode}`)
+    io.to(roomCode).emit('updateGame', { player, board })
   })
 
   socket.on('disconnect', () => {
